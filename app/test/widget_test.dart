20c5594333d2611695357d2216c4e6ae871629a4
Yasser Idbouzkri <idbouzkriy@gmail.com>
2026-07-06 16:39:07 +0800

    test: add flutter smoke test

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,9 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:cpeb_app/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('app renders dashboard title', (tester) async {
+  testWidgets('renders dashboard title', (tester) async {
     await tester.pumpWidget(const CpebApp());
+
     expect(find.text('University Equipment Booking System'), findsOneWidget);
   });
 }
